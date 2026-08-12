7233684e4d32f2c63e60be5e36234a31d9c38a3d
Naz <nazareth.dnd@gmail.com>
2026-08-12 16:54:35 +0100

    explicit appuser app dir ownership on docker image for compatibility across different host os

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,9 @@
 FROM python:3.12-alpine
 WORKDIR /app
 
-# Create store file as root user before switching to non-root
-RUN touch ./store.json
-
 # create a non-root user to run the application
 RUN adduser -D -u 1001 -s /bin/sh app
-RUN chown app:app ./store.json
+RUN chown app:app /app
 USER app
 
 RUN pip install --user --no-cache-dir "poetry==2.4.1"
--- daily_brief/src/dbrief/constants.py
+++ daily_brief/src/dbrief/constants.py
@@ -5,11 +5,12 @@ T212_BASE_URL = getenv('TRADING212_BASE_URL')
 
 # GUI configuration
 GUI_HOST = getenv('GUI_HOST')
-GUI_PORT = getenv('GUI_PORT')
+DEFAULT_GUI_PORT = getenv('DEFAULT_GUI_PORT', 8001)
+GUI_PORT = getenv('GUI_PORT', DEFAULT_GUI_PORT)
 
 # Server configuration
 SERVER_HOST = '0.0.0.0'
-SERVICES_DEFAULT_PORT = getenv('SERVICES_DEFAULT_PORT', 8002)
+SERVICES_DEFAULT_PORT = getenv('DEFAULT_SERVICES_PORT', 8002)
 SERVICES_PORT = int(getenv('SERVICES_PORT', SERVICES_DEFAULT_PORT))
 
 # Delay (s) before executing a callback task after the main task completes
